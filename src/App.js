066ced342390a36359aeb44d31bf846863c95c5d
shivraj khetri <shivrajkhetri7@gmail.com>
2021-04-23 13:53:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Footer from "../src/DemoFooter/footer"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    <div className="App">
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DemoFooter/footer.jsx
@@ -0,0 +1,228 @@
+import React from "react";
+import "./style.css"
+// import { Link, withRouter } from "react-router-dom";
+// import HomeWorkIcon from "@material-ui/icons/HomeWork";
+
+const Footer = () => {
+    return (
+
+        <div className="footer">
+            {/* Get Right Property Headding Header */}
+            <div className="footer-container-head">
+                <div className="row">
+                    <div className="col-md-12 col-sm-12">
+
+                        <div className="footer__titleOneHeader">
+                            <p
+                                className="header__logo"
+                                style={{
+                                    marginLeft: "10px",
+                                    marginRight: "10px",
+                                    marginTop: "10px",
+                                }}><span
+                                    style={{
+                                        color: "#4bd80a",
+                                        marginTop: "0px",
+                                        fontSize: "22px",
+                                        marginRight: ".15rem",
+                                    }}
+                                >G</span>
+                                <span className="header_name" style={{
+                                    color: "white",
+                                }}>etRightProperty.Com</span>
+                            </p>
+                        </div>
+                        {/* <p
+                            className="header__logoItemPart"
+                        > */}
+                        <p
+                            className="header__logoItemPart"
+                            style={{
+                                marginLeft: "10px",
+                                marginTop: "10px",
+                            }}
+                        >
+                            GetRightProperty is a new home rental platform that makes it easier
+                            to find your new home without paying any brokerage. Use our verified
+                            listing to ensure you find your dream home by contacting the owners
+                            directly. In case you cannot find a property you are looking for,
+                            please post a requirement and we'll send you email notification with
+                            properties matching your requirements.
+                      </p>
+                    </div>
+                </div>
+            </div>
+            {/*  Get Right Property second Box   */}
+            <div className="footer-second-section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="row pt-2">
+                                <div className="col-md-6 col-6">
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <div className="footer-parent-div">
+                                                <div className="footer-child-div">
+                                                    <ul>
+                                                        <li>Real estate in Mumbai</li>
+                                                        <li>Real estate in Bengaluru</li>
+                                                        <li>Real estate in Hyderabad</li>
+                                                        <li>Real estate in Pune</li>
+                                                        <li>Real estate in Chennai</li>
+                                                        <li>Real estate in Delhi</li>
+                                                        <li>Real estate in Gurgaon</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <div className="footer-parent-div">
+                                                <div className="footer-child-div">
+                                                    <ul>
+                                                        <li>Real estate in Gurgaon</li>
+                                                        <li>Real estate in Noida</li>
+                                                        <li>Real estate in Kolkata</li>
+                                                        <li>Real estate in Goa</li>
+                                                        <li>Real estate in Thane</li>
+                                                        <li>Real estate in Panjab</li>
+                                                        <li>Real estate in Faridabad</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 col-6">
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <div className="footer-parent-div">
+                                                <div className="footer-child-div">
+                                                    <ul>
+                                                        <li>Real estate in Mumbai</li>
+                                                        <li>Real estate in Bengaluru</li>
+                                                        <li>Real estate in Hyderabad</li>
+                                                        <li>Real estate in Pune</li>
+                                                        <li>Real estate in Chennai</li>
+                                                        <li>Real estate in Delhi</li>
+                                                        <li>Real estate in Gurgaon</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <div className="footer-parent-div">
+                                                <div className="footer-child-div">
+                                                    <ul>
+                                                        <li>Real estate in Gurgaon</li>
+                                                        <li>Real estate in Noida</li>
+                                                        <li>Real estate in Kolkata</li>
+                                                        <li>Real estate in Goa</li>
+                                                        <li>Real estate in Thane</li>
+                                                        <li>Real estate in Panjab</li>
+                                                        <li>Real estate in Faridabad</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            {/*  Get Right Property third Box  */}
+            <div className="footer-third-section pt-5">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="row">
+                            <div className="col-md-4 col-4">
+                                <div className="footer-parent-div">
+                                    <div className="footer-child-div">
+                                        <h6 className="text-center footer-sub-headding">
+                                            <span
+                                                style={{
+                                                    color: "#4bd80a",
+                                                    marginTop: "0px",
+                                                    fontWeight: 600,
+                                                }}
+                                            >C</span>
+                                ompany</h6>
+                                        <ul className="">
+                                            <li>Career</li>
+                                            <li>About Use</li>
+                                            <li>Terms</li>
+                                            <li>Private Policy</li>
+                                            <li>Contact Us</li>
+                                            <li>Unsubscribe</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-4">
+                                <div className="footer-parent-div">
+                                    <div className="footer-child-div">
+                                        <ul className="">
+                                            <h6 className="text-center footer-sub-headding">
+                                                <span
+                                                    style={{
+                                                        color: "#4bd80a",
+                                                        marginTop: "0px",
+                                                        fontWeight: 600,
+                                                    }}
+                                                >E</span>
+                                        xplore</h6>
+                                            <li>News</li>
+                                            <li>Home Loan</li>
+                                            <li>Sitemap</li>
+                                            <li>Blog</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-4">
+                                <div className="footer-parent-div">
+                                    <div className="footer-child-div">
+                                        <h6 className="text-center footer-sub-headding">
+                                            <span
+                                                style={{
+                                                    color: "#4bd80a",
+                                                    marginTop: "0px",
+                                                    fontWeight: 600,
+                                                }}
+                                            >F</span>
+                                        ollow</h6>
+                                        <ul className="">
+                                            <li>Facebook</li>
+                                            <li>Instagram</li>
+                                            <li>Twitter</li>
+                                            <li>LinkedIn</li>
+                                            <li>Pinterest</li>
+                                            <li>Youtube</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/*  Get Right Property four Box  */}
+            <div className="footer-four-section">
+                <div className="row">
+                    <div className="col-md-12 col-12">
+                        <div className="footer__containerFour">
+                            <p style={{ marginTop: "15px" }}>
+                                &copy; Get right property 2020 All Rights Reserved
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    );
+};
+export default Footer;
